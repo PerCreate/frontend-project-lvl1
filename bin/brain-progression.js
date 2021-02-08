b7a294271cf6d60379a3478df36e2a41ce5de983
PerCreate <kostya.pershin007@gmail.com>
2021-02-08 13:24:31 +0400

    eighthStep

---
+++ bin/brain-progression.js
@@ -0,0 +1,20 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+import readlineSync from 'readline-sync';
+import progression from '../logic-game.js';
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello,${userName}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const question1 = String('15 20 25 30 ... 40 45 50');
+const question2 = String('30 33 36 ... 42 45 48');
+const question3 = String('22 30 38 ... 54 62 70');
+const answer1 = String('35');
+const answer2 = String('39');
+const answer3 = String('46');
+
+progression(question1, answer1, question2, answer2, question3, answer3, userName);
